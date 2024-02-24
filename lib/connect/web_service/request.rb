e589668db3734fe083e5fe022e342b73d3c9604f
Anatoly Busygin <anatolyb94@gmail.com>
2024-02-25 00:43:33 +0500

    changed variables name

--- lib/connect/web_service/request.rb
+++ lib/connect/web_service/request.rb
@@ -4,21 +4,21 @@ module Connect
   module WebService
     module Request
       def call(client, options)
-        params = []
-        params = options[:params] if options[:params]
+        request_params = []
+        request_params = options[:params] if options[:params]
         method = options[:method]
         method_as_s = method.to_s
         camel_case_method_name = method_as_s.to_s.gsub(/(?:_|\b)([a-z])/) { $1.upcase }
-        xml_body = build_params(camel_case_method_name, params)
+        xml_body = build_params(camel_case_method_name, request_params)
         xml = xml_template(xml_body)
         response = client.connection.call(method, xml: xml)
         body = response.body
         body_key = "#{method_as_s}_response".to_sym
-        result = []
+        response_data = []
         if (property = body.dig(body_key, :return, :property)) && property.size > 1 && property[1][:value][:row]
-          result = property[1][:value][:row]
+          response_data = property[1][:value][:row]
         end
-        result
+        response_data
       end
 
       private
